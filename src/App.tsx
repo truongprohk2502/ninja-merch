db9c72ceca286f0b811664e365b95263fefb0525
Nguyen Dinh Truong <nguyendinhtruong98@gmail.com>
2023-05-15 22:19:27 +0700

    Add component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import type { Component } from "solid-js";
+import Card from "./components/Card";
+import banner from "./assets/banner.png";
 
 const App: Component = () => {
-  return <div>Home</div>;
+  console.log("hello, ninjas!");
+
+  return (
+    <div>
+      <header>
+        <h1>Ninja Merch</h1>
+      </header>
+
+      <img src={banner} alt="site banner" />
+
+      <Card />
+      <Card />
+      <Card />
+    </div>
+  );
 };
 
 export default App;
---
+++ src/components/Card.tsx
@@ -0,0 +1,14 @@
+import type { Component } from "solid-js";
+
+const Card: Component = () => {
+  return (
+    <div>
+      <h2>Card Component</h2>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere, enim?
+      </p>
+    </div>
+  );
+};
+
+export default Card;
